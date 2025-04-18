64bd4030191a8a1ae1d3a231ec6453126123de4c
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-17 21:29:45 -0300

    agregue enuncido y complete ejercicio

---
+++ js/ejercicio8.js
@@ -0,0 +1,24 @@
+/*
+Crea script para generar pirámide siguiente con los números del 1 al número
+que indique el usuario (no mayor de 50)
+*/
+
+const entrada = prompt("Ingrese un número (máximo 50)");
+
+if (!entrada === null || !isNaN(entrada)) {
+  const numero = parseInt(entrada);
+
+  if (numero > 0 && numero <= 50) {
+    for(let i=1; i<=numero; i++){
+        let numero = "";
+        for(let j=0; j<i; j++){
+            numero += i;
+        }
+        document.writeln(`<p>${numero}</p>`);
+    }
+  }else {
+    alert("Fin del programa. Entrada no valida");
+  }
+} else {
+  alert("Fin del programa. Entrada no valida");
+}
