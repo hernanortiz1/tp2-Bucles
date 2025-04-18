cb05bde1ebddd5ca82c7942fad4511479c7b0449
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-18 20:14:02 -0300

    agregue ciclo do while y salto de linea entre tablas

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -4,28 +4,30 @@ Dentro de cada una de las celdas deberá escribirse un número consecutivo en
 orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 */
 
-const fila = prompt("Ingrese número de filas");
-const columna = prompt("Ingrese número de columnas");
+do {
+  const fila = prompt("Ingrese número de filas");
+  const columna = prompt("Ingrese número de columnas");
 
-if (fila !== null && columna !== null && !isNaN(fila) && !isNaN(columna)) {
-  const numFila = parseInt(fila);
-  const numColumna = parseInt(columna);
-  const totalCeldas = numFila * numColumna;
+  if (fila !== null && columna !== null && !isNaN(fila) && !isNaN(columna)) {
+    const numFila = parseInt(fila);
+    const numColumna = parseInt(columna);
+    const totalCeldas = numFila * numColumna;
 
-  let contador = totalCeldas;
+    let contador = totalCeldas;
 
-  document.writeln("<table border='1' cellspacing='0' cellpadding='5'>");
+    document.writeln("<table border='1' cellspacing='0' cellpadding='5'>");
 
-  for (let i = 0; i < numFila; i++) {
-    document.writeln("<tr>");
-    for (let j = 0; j < numColumna; j++) {
-      document.writeln(`<td>${contador}</td>`);
-      contador--;
+    for (let i = 0; i < numFila; i++) {
+      document.writeln("<tr>");
+      for (let j = 0; j < numColumna; j++) {
+        document.writeln(`<td>${contador}</td>`);
+        contador--;
+      }
+      document.writeln("</tr>");
     }
-    document.writeln("</tr>");
-  }
 
-  document.writeln("</table>");
-} else {
-  alert("Entrada no válida. Debe ingresar números.");
-}
+    document.writeln("</table> <br>");
+  } else {
+    alert("Entrada no válida. Debe ingresar números.");
+  }
+} while (confirm("¿Desea ingresar más datos?"));
