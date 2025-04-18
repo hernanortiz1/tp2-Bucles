5abd09041ed4cf85d6b84c88dec9dd1f0af7aac9
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-18 20:05:15 -0300

    agregue ciclo do while

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -4,22 +4,25 @@ que indique el usuario (no mayor de 50) de la siguiente forma : (suponiendo que
 indica 30).
 */
 
-const entrada = prompt("Ingrese un número (máximo 50)");
 
-if (!entrada === null || !isNaN(entrada)) {
-  const numero = parseInt(entrada);
-
-  if (numero > 0 && numero <= 50) {
-    for (let i = numero; i > 0; i--) {
-      let numero = "";
-      for (let j = 0; j < i; j++) {
-        numero += i;
+do{
+  let entrada = prompt("Ingrese un número (máximo 50)");
+  if (!entrada === null || !isNaN(entrada)) {
+    const numero = parseInt(entrada);
+  
+    if (numero > 0 && numero <= 50) {
+      for (let i = numero; i > 0; i--) {
+        let numero = "";
+        for (let j = 0; j < i; j++) {
+          numero += i;
+        }
+        document.writeln(`<p>${numero}</p>`);
       }
-      document.writeln(`<p>${numero}</p>`);
+    }else {
+      alert("Fin del programa. Entrada no valida");
     }
-  }else {
+  } else {
     alert("Fin del programa. Entrada no valida");
   }
-} else {
-  alert("Fin del programa. Entrada no valida");
-}
+}while(confirm("¿Desea ingresar otro número?"))
+
